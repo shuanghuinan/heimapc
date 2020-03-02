36ce224e94ba182ba1732209f46d8902da9904b9
yourname <shuanghuinan@163.com>
2020-03-02 22:04:58 +0800

    安装axios,配置全局使用

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import '@/styles/index.less'
+import ElementUI from 'element-ui' // 引入elementui组件库
+import 'element-ui/lib/theme-chalk/index.css'// 引入组件库的css样式文件
+import '@/styles/index.less'// 引入公共样式文件
+import axios from 'axios'// 引入axios包
 Vue.use(ElementUI)
-
+Vue.prototype.$axios = axios// 将axios赋值给Vue对象的原型对象
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'// 配置公共的请求头
 Vue.config.productionTip = false
 
 new Vue({
