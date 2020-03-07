fa19e773b28a0bad471c4a7fa5680c13294a6520
yourname <shuanghuinan@163.com>
2020-03-07 13:40:42 +0800

    素材列表的收藏素材/取消收藏/删除素材

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -11,15 +11,19 @@
       </el-upload>
     </el-row>
     <!-- 标签页 -->
-    <el-tabs type="card" v-model="cur" @tab-click="changeTab">
+    <el-tabs type="card" v-model="cur" @tab-click="changeTab" v-loading='loading'>
       <!-- 全部 -->
       <el-tab-pane label="全部" name="all">
         <div class="card_list">
           <el-card class="list-item" v-for="item in list" :key="item.id">
             <img :src="item.url" alt />
             <el-row class="operate" type="flex" justify="center" align="middle">
-              <i class="el-icon-star-off"></i>
-              <i class="el-icon-delete"></i>
+              <i
+                class="el-icon-star-on"
+                @click="collectOrCancel(item)"
+                :style="{color: item.is_collected ? 'red' : 'black'}"
+              ></i>
+              <i class="el-icon-delete-solid" @click="delItem(item)"></i>
             </el-row>
           </el-card>
         </div>
@@ -53,18 +57,20 @@ export default {
   data () {
     return {
       cur: 'all', // 当前所激活的页签
-      list: [], // 全部素材的信息
+      list: [], // 全部素材的信息,有id,url和is_collect
       // 放页码信息
       page: {
         total_count: 1, // 总的条数,默认为1
         page: 1, // 当前页数,默认是1
         per_page: 3 // 每页显示条数,默认8条
-      }
+      },
+      loading: false// 用来表示是否在加载中
     }
   },
   methods: {
     // 此方法用来获取素材信息
     getMaterial () {
+      this.loading = true
       this.$axios({
         url: '/user/images',
         params: {
@@ -75,7 +81,8 @@ export default {
       }).then(res => {
         this.list = res.data.results // 如果请求成功的话,就将响应的results数组的值赋给list变量
         this.page = res.data // 解构赋值
-        // console.log(res)
+        this.loading = false
+        console.log(res)
       })
     },
 
@@ -109,7 +116,38 @@ export default {
         .catch(() => {
           this.$message.error('上传素材失败')
         })
+    },
+
+    // 此方法用来收藏或取消收藏
+    collectOrCancel (item) {
+      this.$axios({
+        url: `/user/images/${item.id}`,
+        method: 'PUT',
+        data: {
+          collect: !item.is_collected
+        }
+      }).then(() => {
+        // console.log(item)
+        this.getMaterial()
+      }).catch(() => {
+        this.$message.error('操作失败')
+      })
+    },
+
+    // 此方法用来删除素材
+    delItem (item) {
+      this.$confirm('您确定要删除此素材吗').then(() => {
+        this.$axios({
+          url: `/user/images/${item.id}`,
+          method: 'DELETE'
+        }).then(() => {
+          this.getMaterial()
+        }).catch(() => {
+          this.$message.error('删除失败')
+        })
+      })
     }
+
   },
   created () {
     this.getMaterial()
@@ -126,7 +164,9 @@ export default {
     justify-content: center;
     // background-color:rgb(211, 100, 57);
     .list-item {
-      background-color: rgb(229, 233, 232);
+      // background: url('../../assets/img/item.jpg') center no-repeat;
+      // background-size: cover;
+      box-shadow: 6px 6px 9px -5px rgba(115, 183, 204, 0.644);
       position: relative;
       width: 300px;
       height: 400px;
