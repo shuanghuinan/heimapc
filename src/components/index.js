429d82e1e6d5955f816c16de1571bfd86d4111b8
yourname <shuanghuinan@163.com>
2020-03-09 02:20:03 +0800

    富文本框的下载和引入

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import LayoutAside from '@/components/home/layout-aside'
 import LayoutHeader from '@/components/home/layout-header'
 import BreadCrumb from '@/components/common/bread-crumb'
+import VueQuillEditor from 'vue-quill-editor'
 export default {
   install (Vue) {
     Vue.component('layout-aside', LayoutAside)
     Vue.component('layout-header', LayoutHeader)
     Vue.component('bread-crumb', BreadCrumb)
+    Vue.use(VueQuillEditor)
   }
 }
