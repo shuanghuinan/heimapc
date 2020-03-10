20c54b8df5450d7a695838e06c706871dc00d9de
yourname <shuanghuinan@163.com>
2020-03-10 22:29:32 +0800

    在导航守卫里面引入页面进度条

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,6 +1,9 @@
 // 导航守卫
-import router from '@/router'
+import router from '@/router'// 引入路由表
+import nprogress from 'nprogress'// 引入页面进度条
+import 'nprogress/nprogress.css'// 页面进度条的样式文件
 router.beforeEach(function (to, from, next) {
+  nprogress.start()
   // 如果要去的地址是以home开头,即:要去的地址是主页
   if (to.path.startsWith('/home')) {
     // 则先判断有没有token
@@ -16,4 +19,7 @@ router.beforeEach(function (to, from, next) {
     // 如果要去的地址不是以home开头,直接放过
     next()
   }
+})
+router.afterEach(function (to, from, next) {
+  setTimeout(() => nprogress.done(), 1000)
 })
