c447c1305964d629c8b85a29df5326d559909c08
yourname <shuanghuinan@163.com>
2020-03-10 20:13:47 +0800

    个人信息页面的路由挂载

--- src/router/index.js
+++ src/router/index.js
@@ -26,8 +26,8 @@ const routes = [
       { path: 'material', component: () => import('@/views/material') }, // 引入素材组件,按需加载
       // { path: 'articles', component: () => import('@/views/articles') } // 引入文章(内容)组件,按需加载
       { path: 'articles', component: () => import('@/views/articles/index_two') }, // 引入文章(内容)组件,按需加载
-      { path: 'publish:id?', component: () => import('@/views/publish') }
-
+      { path: 'publish:id?', component: () => import('@/views/publish') }, // 引入文章发表组件(按需加载)
+      { path: 'account', comment: () => import('@/views/account') }
     ]
   }
 
